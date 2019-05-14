5fcb455ae2e83feb3346362eea8d56b00d65fb17
auravadima <auravadima@gmail.com>
2019-05-15 01:21:06 +0700

    Posts

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ed51b96c0ade898dc858b8263f01433a'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
